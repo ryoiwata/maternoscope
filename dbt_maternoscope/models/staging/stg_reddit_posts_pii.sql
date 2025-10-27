b7276dfd06b28e08c2461202ef216faef7e6966f
ryoiwata <ryoiwata@github.com>
2025-10-27 15:32:31 -0500

    feat: removed english language check

--- dbt_maternoscope/models/staging/stg_reddit_posts_pii.sql
+++ dbt_maternoscope/models/staging/stg_reddit_posts_pii.sql
@@ -21,36 +21,38 @@ pii_redacted as (
     score,
     num_comments,
     scraped_at,
-    content_hash,
+    content_hash,                      
 
     -- Keep raw text unaltered
     post_content_raw as text_raw,
 
-    -- PII redaction chain: URLs, emails, phones, usernames, subreddits
+    -- PII redaction chain (use Snowflake regex parameters for case-insensitive: 'i')
     regexp_replace(
       regexp_replace(
         regexp_replace(
           regexp_replace(
             regexp_replace(
-              regexp_replace(
-                regexp_replace(post_content_raw, '(?i)http://[^\\s]+', '[URL]'),
-                '(?i)https://[^\\s]+', '[URL]'
-              ),
-              '(?i)www\\.[^\\s]+', '[URL]'
+              -- URLs (http/https/www)
+              regexp_replace(post_content_raw, 'https?://\\S+|\\bwww\\.[^\\s]+', '[URL]', 1, 0, 'i'),
+              -- emails
+              '[a-z0-9][a-z0-9._%+-]*@[a-z0-9][a-z0-9.-]*\\.[a-z]{2,}', '[EMAIL]', 1, 0, 'i'
             ),
-            '(?i)[a-z0-9][a-z0-9._%+-]*@[a-z0-9][a-z0-9.-]*\\.[a-z]{2,}', '[EMAIL]'
+            -- phones
+            '(\\+?[0-9][0-9-\\s\\(\\)]{7,}[0-9])', '[PHONE]', 1, 0
           ),
-          '(?i)(\\+?[0-9][0-9-\\s\\(\\)]{7,}[0-9])', '[PHONE]'
+          -- usernames u/...
+          '\\bu/[a-z0-9_\\-]+', 'u/[USER]', 1, 0, 'i'
         ),
-        '(?i)\\bu/[a-z0-9_\\-]+', 'u/[USER]'
+        -- subreddits r/...
+        '\\br/[a-z0-9_]+', 'r/[SUB]', 1, 0, 'i'
       ),
-      '(?i)\\br/[a-z0-9_]+', 'r/[SUB]'
+      -- optional: collapse whitespace
+      '\\s+', ' ', 1, 0
     ) as text_for_llm
 
   from base
 ),
 
--- Compute metrics
 with_metrics as (
   select
     *,
@@ -58,27 +60,20 @@ with_metrics as (
     array_size(split(text_for_llm, ' ')) as n_words,
     greatest(regexp_count(text_for_llm, '[\\.!?]+'), 1) as n_sentences,
 
-    -- Punctuation metrics
+    -- punctuation metrics
     regexp_count(text_for_llm, '\\?') as n_questions,
     regexp_count(text_for_llm, '!') as n_exclaims,
 
-    -- Redaction counts (from redacted text)
+    -- redaction counts (match the tokens)
     regexp_count(text_for_llm, '\\[URL\\]')   as n_urls_redacted,
     regexp_count(text_for_llm, '\\[EMAIL\\]') as n_emails_redacted,
     regexp_count(text_for_llm, '\\[PHONE\\]') as n_phones_redacted,
     regexp_count(text_for_llm, 'u/\\[USER\\]') as n_usernames_redacted,
     regexp_count(text_for_llm, 'r/\\[SUB\\]')  as n_subreddits_redacted,
 
-    -- Token estimate (~4 chars/token)
-    ceil(length(text_for_llm) / 4.0) as token_estimate,
+    -- token estimate (~4 chars/token)
+    ceil(length(text_for_llm) / 4.0) as token_estimate
 
-    -- Language hint (crude: proportion non-ASCII)
-    case
-      when length(text_for_llm) > 0
-           and (length(regexp_replace(text_for_llm, '[\\x00-\\x7F]', ''))::float
-                / length(text_for_llm)) < 0.10
-      then 'en' else 'unknown'
-    end as lang_hint
   from pii_redacted
 ),
 
@@ -86,8 +81,7 @@ final as (
   select
     *,
     case
-      when lang_hint = 'en'
-       and n_words between 5 and 1000
+      when n_words between 5 and 1000
        and token_estimate < 4000
        and text_for_llm is not null
       then true else false
